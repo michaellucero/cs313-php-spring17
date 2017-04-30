1c0ac96325fe48b4bdda050b100245fd8d5fb546
Michael Lucero <luc14006@byui.edu>
2017-04-29 22:42:04 -0600

    added home page

---
+++ web/homepage/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <title>My Home page</title>
+  </head>
+  <body>
+    <div class="header">
+      <h1>Welcome to my Home Page</h1>
+      <span class="linkfont">
+        <ul>
+          <li class="linkhover"><a href="#">Home</a></li>
+          <li class="linkhover"><a href="#">Assignments</a></li>
+          <li class="linkhover"><a href="#">Comming Soon</a></li>
+          <li class="linkhover"><a href="#">Comming Soon</a></li>
+        </ul>
+      </span>
+    </div>
+      <div class="background">
+        <div class="mypicture">
+          <img src="mypic.jpg" alt="Picture of Me." style="width: 150px; height: 150px">
+          <p class="textleft">Hello everyone, My name is Michael Lucero. I am super excited this semester to be apart of CS 313 where
+             I plan to learn some amazing stuff. I am a student studying Computer Science. I am a big fan of biking and
+             the outdoors.</p>
+          <p class="textright">This page is going to grow and will be added to throughout the semester. The image that you see was a
+          creation I came up with to show that light will always shine into ones life.</p>
+        </div>
+      </div>
+      <div class="footer">
+        <h5>&copy 2017 | Created by Michael Lucero</h5>
+
+      </div>
+  </body>
+</html>
